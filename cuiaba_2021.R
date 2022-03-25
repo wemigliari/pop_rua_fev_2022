9303809a308e43d01f9f1a98bb05b0ce583789fb
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2022-03-25 22:52:28 +0100

    Add files via upload

--- cuiaba_2021.R
+++ cuiaba_2021.R
@@ -71,9 +71,7 @@ mds_fam_2021 <- data.frame(count(munic_2021 , "IN_PARC_MDS_FAM"))
 
 mds_fam_2021 <- mds_fam_2021%>% replace_na(list(IN_PARC_MDS_FAM = "Sem Dados"))
 
-mds_fam_2021$IN_PARC_MDS_FAM[which(mds_fam_2021$IN_PARC_MDS_FAM=="0")] <- "Sem Dados"
-
-mds_fam_2021$IN_PARC_MDS_FAM <- gsub(c("000"), c("Nenhuma"), mds_fam_2021$IN_PARC_MDS_FAM)  
+mds_fam_2021$IN_PARC_MDS_FAM[which(mds_fam_2021$IN_PARC_MDS_FAM=="0")] <- "Nenhuma"
 
 mds_fam_2021$IN_PARC_MDS_FAM <- gsub(c("101"), c("Família Cigana"), mds_fam_2021$IN_PARC_MDS_FAM)  
 
