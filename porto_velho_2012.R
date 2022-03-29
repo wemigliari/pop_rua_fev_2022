1f085b051e708aaad5f67d8a711a584d96401c7d
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2022-03-29 23:53:03 +0200

    Add files via upload

--- porto_velho_2012.R
+++ porto_velho_2012.R
@@ -5,6 +5,7 @@ library(stringr)
 library(readr)
 library(tidyverse)
 library(writexl)
+library(xlsx)
 
 
 ### Tabela 2012 do Ministério das Cidades
@@ -131,7 +132,8 @@ write.xlsx(esta_cadas_2012, "/Users/wemigliari/Documents/R/tabelas/min_cid_obser
 ###8 Seleção de Dados pa CO_SEXO_PESSOA
 sexo_2012 <- data.frame(count(munic_2012 , "CO_SEXO_PESSOA"))
 
-sexo_2012$CO_SEXO_PESSOA <- sexo_2012$CO_SEXO_PESSOA[sexo_2012$CO_SEXO_PESSOA==c(1, 2)]<-c("Masculino", "Feminino")
+sexo_2012$CO_SEXO_PESSOA[which(sexo_2012$CO_SEXO_PESSOA=="1")] <- "Masculino"
+sexo_2012$CO_SEXO_PESSOA[which(sexo_2012$CO_SEXO_PESSOA=="2")] <- "Feminino"
 
 write.xlsx(sexo_2012, "/Users/wemigliari/Documents/R/tabelas/min_cid_observatorio/porto_velho/2012.xlsx",
            sheetName="Sexo", append=TRUE)
