3f56784505115097c597db023a5f1edd7895d374
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2022-05-30 21:49:04 +0200

    Add files via upload

---
+++ estados_pop_rua_2019.R
@@ -0,0 +1,47 @@
+library(readxl)
+library(dplyr)
+library(plyr)
+library(stringr)
+library(readr)
+library(tidyverse)
+library(writexl)
+library(RColorBrewer)
+library(leaflet)
+library(rgdal)
+library(sf)
+
+
+### Tabela 2019 do Ministério das Cidades
+
+brasil_munic_2019  <- read_csv("/Users/wemigliari/Documents/R/tabelas/pop_rua_min_cidades/TB_POP_RUA_201912.csv", 
+                               col_names = TRUE)
+
+names(brasil_munic_2019)[1]<- "codigo_ibge"
+
+munic_2019_pop_rua  <- data.frame(count(brasil_munic_2019, "codigo_ibge"))
+
+######### Municípios
+
+brasil_municipios <- read_sf("/Users/wemigliari/Documents/R/data/BR_municipios_2021.shp")
+names(brasil_municipios)[1] <- "codigo_ibge"
+
+brasil_municipios_2019_pop_rua <- data <- merge(munic_2019_pop_rua, brasil_municipios, by = "codigo_ibge")
+
+brasil_municipios_2019_pop_rua <- brasil_municipios_2019_pop_rua%>%
+  select(codigo_ibge, freq, NM_MUN, SIGLA)
+
+######### Estados
+
+brasil_municipios_2019_pop_rua$NM_MUN <- NULL
+
+brasil_estados_2019_pop_rua<- aggregate(brasil_municipios_2019_pop_rua["freq"],
+                                        by=brasil_municipios_2019_pop_rua["SIGLA"],
+                                        sum)
+
+sum(brasil_municipios_2019_pop_rua$freq)
+
+###### Para Datawrapper
+
+write_xlsx(brasil_estados_2019_pop_rua, "/Users/wemigliari/Documents/R/tabelas/min_cid_observatorio/estados_pop_rua_2019.xlsx")
+
+######
