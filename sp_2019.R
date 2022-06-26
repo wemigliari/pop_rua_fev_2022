e33b6dfe9d99d98b0f0a8a077d52efacecd30006
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2022-06-26 22:10:08 +0200

    Add files via upload

--- sp_2019.R
+++ sp_2019.R
@@ -138,7 +138,7 @@ write.xlsx(ler_escrever_2019, "/Users/wemigliari/Documents/R/tabelas/min_cid_obs
 ###13 Seleção de Dados SP GRAU_INSTRUCAO
 instrucao_2019 <- data.frame(count(munic_2019 , "GRAU_INSTRUCAO"))
 
-instrucao_2019$GRAU_INSTRUCAO <- instrucao_2019$GRAU_INSTRUCAO[instrucao_2019$GRAU_INSTRUCAO==c(0, 1, 2, 3, 4, 5, 6)]<-c("Sem Dados", "Sem Instrução", "Fundamental Incompleto", "Fundamental Completo", "Ensino Médio Incompleto", "Ensino Médio Completo", "Superior Completo")
+instrucao_2019$GRAU_INSTRUCAO <- instrucao_2019$GRAU_INSTRUCAO[instrucao_2019$GRAU_INSTRUCAO==c(0, 1, 2, 3, 4, 5, 6)]<-c("Sem Dados", "Sem Instrução", "Fundamental Incompleto", "Fundamental Completo", "Ensino Médio Incompleto", "Ensino Médio Completo", "Superior Incompleto ou Mais")
 
 write.xlsx(instrucao_2019, "/Users/wemigliari/Documents/R/tabelas/min_cid_observatorio/sp/2019.xlsx",
            sheetName="Instrução", append=TRUE)
